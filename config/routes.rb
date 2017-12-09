481a6a701ff618ef4ade56509d905297918f8acf
marcelo_604 <marcelosa604@gmail.com>
2017-12-09 17:13:33 -0200

    acidionado campo 'doador' ao produto

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
 
-  resources :produtos
+  resources :products
   get 'como_funciona/index'
 
   devise_for :users
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171209173258) do
+ActiveRecord::Schema.define(version: 20171209190028) do
+
+  create_table "products", force: :cascade do |t|
+    t.string "nome"
+    t.string "tipo"
+    t.string "estado"
+    t.text "descricao"
+    t.string "doador"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "produtos", force: :cascade do |t|
     t.string "nome"
@@ -24,6 +34,9 @@ ActiveRecord::Schema.define(version: 20171209173258) do
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
+    t.string "nome", default: "", null: false
+    t.string "endereco", default: "", null: false
+    t.string "telefone", default: "", null: false
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
