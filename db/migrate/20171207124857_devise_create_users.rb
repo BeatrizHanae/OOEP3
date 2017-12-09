16054e7fd6d92d8301e315f6b95c9d77623596a2
Beatriz Hanae <beatriz.hanae@gmail.com>
2017-12-09 20:12:33 -0200

    Adicionando mais parâmetros para cadastro de usuários

--- db/migrate/20171207124857_devise_create_users.rb
+++ db/migrate/20171207124857_devise_create_users.rb
@@ -2,11 +2,11 @@ class DeviseCreateUsers < ActiveRecord::Migration[5.1]
   def change
     create_table :users do |t|
       ## Database authenticatable
-      t.string :email,              null: false, default: ""
-      t.string :encrypted_password, null: false, default: ""
-t.string :nome,              null: false, default: ""
-t.string :endereco,              null: false, default: ""
-t.string :telefone,              null: false, default: ""
+      t.string :email,                 null: false, default: ""
+      t.string :encrypted_password,    null: false, default: ""
+      t.string :nome,                  null: false, default: ""
+      t.string :endereco,              null: false, default: ""
+      t.string :telefone,              null: false, default: ""
 
       ## Recoverable
       t.string   :reset_password_token
