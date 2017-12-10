618c144c2624d92fcdb0280826088155c9230658
marcelo_604 <marcelosa604@gmail.com>
2017-12-10 08:52:32 -0200

    adicionado fotos a produtos

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -69,6 +69,6 @@ class ProductsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def product_params
-      params.require(:product).permit(:nome, :tipo, :estado, :descricao, :doador)
+      params.require(:product).permit(:nome, :tipo, :estado, :descricao, :doador, :avatar)
     end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171209190028) do
+ActiveRecord::Schema.define(version: 20171210044051) do
 
   create_table "products", force: :cascade do |t|
     t.string "nome"
@@ -20,6 +20,10 @@ ActiveRecord::Schema.define(version: 20171209190028) do
     t.string "doador"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "avatar_file_name"
+    t.string "avatar_content_type"
+    t.integer "avatar_file_size"
+    t.datetime "avatar_updated_at"
   end
 
   create_table "produtos", force: :cascade do |t|
